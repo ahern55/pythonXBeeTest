56bf73095acbc3c03d20418872d77607b5b6a7c2
Jason Ahern <jason.ahern53@gmail.com>
2022-10-30 18:39:28 -0400

    testing

--- broadcaster.py
+++ broadcaster.py
@@ -1,20 +1,23 @@
 from digi.xbee.devices import XBeeDevice
 import time
 
+
 def main():
-  # Instantiate a local XBee object.
-  xbee = XBeeDevice("COM1", 9600)
-  try:
-    xbee.open()
-  except:
-    print("COM port does not exist or is invalid")
-    return
+    # Instantiate a local XBee object.
+    xbee = XBeeDevice("COM6", 9600)
+    try:
+        xbee.open()
+    except Exception as e:
+        print(e)
+        print("COM port does not exist or is invalid")
+        return
 
-  for _ in range(100):
-    xbee.send_data_broadcast("Hello XBee World!")
-    time.sleep(.1)
+    for _ in range(100):
+        xbee.send_data_broadcast("Hello XBee World!")
+        time.sleep(.1)
 
-  xbee.close()
+    xbee.close()
+
 
 if __name__ == '__main__':
     main()
